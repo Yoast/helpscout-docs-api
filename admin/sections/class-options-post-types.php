d6c9e2f0425779354ec0e770dc9a5a66fbae1aaf
Joost de Valk <joost@yoast.com>
2019-08-29 08:53:24 +0200

    Write our own queries, WP sucks

--- admin/sections/class-options-post-types.php
+++ admin/sections/class-options-post-types.php
@@ -73,44 +73,52 @@ class Options_Post_Types extends Options_Admin implements Options_Section {
 
 	}
 
+	/**
+	 * Renders the indexation section.
+	 *
+	 * @return void
+	 */
 	public function indexation_section() {
 		if ( Options::get( 'api-key' ) === '' || ! isset( $_GET['page'] ) || $_GET['page'] !== 'helpscout-docs-api' ) {
-			return false;
+			return;
 		}
 
-		$non_indexed = [];
-		foreach ( Options::get( 'post-types' ) as $post_type_name => $on ) {
-			$post_type                      = get_post_type_object( $post_type_name );
-			$count                          = wp_count_posts( $post_type_name );
-			$non_indexed[ $post_type_name ] = get_posts(
-				[
-					'fields'         => 'ids',
-					'post_type'      => $post_type_name,
-					'posts_per_page' => 25,
-					'post_status'    => 'publish',
-					'meta_query'     => [
-						[
-							'key'     => '_helpscout_data',
-							'compare' => 'NOT EXISTS',
-						]
-					],
-				] );
+		$enabled_post_types = Options::get( 'post-types' );
+		if ( $enabled_post_types === [] ) {
+			return;
+		}
+
+		global $wpdb;
+
+		$non_indexed_post_ids = [];
+		$non_indexed_count    = [];
+
+		foreach ( $enabled_post_types as $post_type_name => $on ) {
+			$post_type   = get_post_type_object( $post_type_name );
+			$count_total = wp_count_posts( $post_type_name );
+
+			$query                                   = $wpdb->prepare( "SELECT ID FROM $wpdb->posts WHERE post_type = '%s' AND ID NOT IN ( SELECT post_id FROM $wpdb->postmeta WHERE meta_key = '_helpscout_data' )", $post_type_name );
+			$non_indexed_post_ids[ $post_type_name ] = $wpdb->get_col( $query );
+			$non_indexed_count[ $post_type_name ]    = count( $non_indexed_post_ids[ $post_type_name ] );
+
 			echo '<p>';
-			printf( 'There are %d %s, of which %d are not indexed. ', $count->publish, $post_type->labels->name, count( $non_indexed[ $post_type_name ] ) );
-			if ( count( $non_indexed[ $post_type_name ] ) > 0 ) {
+			printf( 'There are %d %s, of which %d are not indexed. ', $count_total->publish, $post_type->labels->name, $non_indexed_count );
+			if ( count( $non_indexed_post_ids[ $post_type_name ] ) > 0 ) {
 				printf( '<a href="' . admin_url( 'options-general.php?page=helpscout-docs-api&index=' . $post_type->name ) . '" class="button">Index %s</a> <br/>', $post_type->labels->name );
 			}
 			echo '</p>';
 		}
 
-		foreach ( Options::get( 'post-types' ) as $post_type_name => $on ) {
+		foreach ( $enabled_post_types as $post_type_name => $on ) {
 			if ( isset( $_GET['index'] ) && $_GET['index'] === $post_type_name ) {
-				foreach ( $non_indexed[ $post_type_name ] as $post_id ) {
-					HelpScout_Article::create( $post_id );
-					HelpScout_Redirect::create( $post_id );
-					echo '.';
-					usleep( 250000 );
-					flush();
+				if ( $non_indexed_count[ $post_type_name ] ) {
+					foreach ( $non_indexed_post_ids as $post_id ) {
+						HelpScout_Article::create( $post_id );
+						HelpScout_Redirect::create( $post_id );
+						echo '.';
+						usleep( 250000 );
+						flush();
+					}
 				}
 			}
 		}
